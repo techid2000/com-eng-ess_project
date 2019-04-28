4963d53761002541db9329a5618d0a1a4c478a18
Techid Janphaka <techid2000@gmail.com>
2019-04-28 17:35:52 +0700

    allowed CORS

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 
 var machine = require('./machine');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/main', (req, res) => {
   res.sendFile('/index.html', {root: __dirname});
 });
 
 app.get('/update', (req, res) => {
+  res.send()
   var degree = parseFloat(req.query.degree);
   var distance = parseFloat(req.query.distance);
   machine.update(degree, distance);
